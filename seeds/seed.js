aef666cab8c6a746b57d296d2bb4a567eb82d508
Cody K <ckietzm2@gmail.com>
2023-11-09 09:17:56 -0600

    Fixed comment functionality

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,8 @@
 const sequelize = require('../config/connection');
-const { User, Project } = require('../models');
+const { User, Comment} = require('../models/');
 
 const userData = require('./userData.json');
-const projectData = require('./projectData.json');
+const commentData = require('./commentData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -12,11 +12,15 @@ const seedDatabase = async () => {
     returning: true,
   });
 
-  for (const project of projectData) {
-    await Project.create({
-      ...project,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
-    });
+  // Use a different variable name (e.g., 'comment') for the loop
+  for (const comment of commentData) {
+    // Use 'Comment' model to create comments
+    await Comment.bulkCreate([
+      {
+        ...comment,
+        user_id: users[Math.floor(Math.random() * users.length)].id,
+      },
+    ]);
   }
 
   process.exit(0);
